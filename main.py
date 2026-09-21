3353e5752b1d177958234a62c51053b006a32f85
u7663394 <guochenwang710@gmail.com>
2026-09-21 20:30:32 +1000

    demo: create a demo FastAPI application

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ from fastapi import FastAPI
 # 1. Create an instance of the FastAPI
 app = FastAPI()
 
-# Comment for testing github email update
+# 2. Define a route for the root endpoint
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+# 3. Run: uvicorn main:app --reload
+#   - uvicorn: a server for running FastAPI applications
+#   - main: refers to the filename
+#   - app: refers to the FastAPI instance
+#   - reload: enables automatic reloading of the server when code changes
