66ad06e3322af52c45be50d21e25208024438c28
u7663394 <guochenwang710@gmail.com>
2026-09-22 19:28:18 +1000

    field: add Field annotation to request body

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 # 1. Create an instance of the FastAPI
 app = FastAPI()
 
 # 2. Define type for request body
 class User(BaseModel):
-  username: str
-  userpwd: str
+  username: str = Field(default="Peter", description="The username of the user.")
+  userpwd: str = Field(..., description="The password of the user.", min_length=5, max_length=20)
 
 # 3. Post
 @app.post("/register")
